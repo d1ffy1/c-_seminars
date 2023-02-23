acadadcee9ae4e00a3fa64509ee2c926ff8dbe86
Vadim Stryukan <vadimstryukan@gmail.com>
2023-02-23 21:46:23 +0200

    initial commit

---
+++ task01/Program.cs
@@ -0,0 +1,16 @@
+﻿/* 1. Напишите программу, которая на вход принимает два
+числа и проверяет, является ли первое число квадратом
+второго.
+a = 25, b = 5 -> да
+a = 2, b = 10 -> нет
+a = 9, b = -3 -> да
+a = -3 b = 9 -> нет */
+
+Console.WriteLine("Введите число А: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число В: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (Math.Sqrt(a)  == b )  
+Console.WriteLine($"Квадрат числа {b} = {a}");
+else 
+Console.WriteLine($"Квадрат числа {b} не = {a}");
---
+++ task03/Program.cs
@@ -0,0 +1,39 @@
+﻿// 3. Напишите программу, которая будет выдавать
+// название дня недели по заданному номеру.
+// 3 -> Среда
+// 5 -> Пятница
+
+Console.WriteLine("Введите число от 1 до 7: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+if (dayNumber == 1)
+{
+    Console.WriteLine("понедельник");
+}
+else if (dayNumber == 2)
+{
+    Console.WriteLine("вторник");
+}
+else if (dayNumber == 3)
+{
+    Console.WriteLine("среда");
+}
+else if (dayNumber == 4)
+{
+    Console.WriteLine("четверг");
+}
+else if (dayNumber == 5)
+{
+    Console.WriteLine("пятница");
+}
+else if (dayNumber == 6)
+{
+    Console.WriteLine("суббота");
+}
+else if (dayNumber == 7)
+{
+    Console.WriteLine("воскресенье");
+}
+else 
+{
+    Console.WriteLine("Неправильный ввод числа");
+}
