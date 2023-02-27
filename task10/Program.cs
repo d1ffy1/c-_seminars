325fc5398fb8d00fe2f8d0e0e4d5dd1c0bf9d52b
Vadim Stryukan <vadimstryukan@gmail.com>
2023-02-28 00:41:42 +0200

    homework after 2nd seminar

---
+++ task10/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трехзначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int secondNumber = SecondNumber(number);
+   Console.WriteLine($"Вторая цифра: {secondNumber}");
+
+
+int SecondNumber(int num)
+{
+    int secondNum1step = num / 10;
+    int secondNum2step = secondNum1step % 10;
+    return secondNum2step;
+}
---
+++ task13/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+Console.WriteLine("Введите натуральное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+while(number > 1000)
+{
+    number /= 10;
+}
+if(number > 99)
+{
+    int thirdDigit = ThirdDigit(number);
+    Console.WriteLine($"Третья цифра = {thirdDigit}");
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+
+
+
+
+int ThirdDigit(int num)
+{
+    int thirdNum = num % 10;
+    return thirdNum;
+}
